32d61867d8b02857e109c84bcc9d62fa45e3dd0c
gnelson85 <gjn33@cornell.edu>
2019-12-05 10:38:46 -0500

    sign up form (buggy)

---
+++ scripts/validation.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $("#join").on("submit", function() {
+    var formValid = true;
+
+    if( $("#name").prop("validity").valid ) {
+        $("#nameError").addClass("hidden");
+    }
+    else {
+      $("#nameError").removeClass("hidden");
+      formValid = false;
+    }
+    if( $("#email").prop("validity").valid ) {
+        $("#emailError").addClass("hidden");
+    }
+    else {
+      $("#emailError").removeClass("hidden");
+      formValid = false;
+    }
+    if( $("#year").val() === "" ) {
+     $("#yearError").removeClass("hidden");
+       formValid = false;
+   }
+   else {
+       $("#yearError").addClass("hidden");
+   }
+    return formValid;
+  });
+});
